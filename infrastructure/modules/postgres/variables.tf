1ad33aee4e9ddcb2086c88af21d22e8b896540a6
lau <lauj@fever.myguest.virtualbox.org>
2025-10-07 21:57:21 +0200

    cicd

--- infrastructure/modules/postgres/variables.tf
+++ infrastructure/modules/postgres/variables.tf
@@ -2,3 +2,8 @@ variable "env" { type = string }
 variable "namespace" { type = string }
 variable "release_name" { type = string }
 variable "chart_version" { type = string }
+module "postgres" {
+  source = "./modules/postgres"
+  db_password = var.postgres_password
+}
+
